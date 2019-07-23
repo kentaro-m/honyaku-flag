75f4e4f55bf7f201f1aecbb73a8e65b1037085b1
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-07-24 02:07:24 +0900

    Add code formatter and linter

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-'use strict';
+"use strict"
 
-import { App } from '@slack/bolt';
-require('dotenv').config();
+import { App } from "@slack/bolt"
+require("dotenv").config()
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+})
+;(async () => {
+  await app.start(process.env.PORT || 3000)
 
-(async () => {
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+  console.log("⚡️ Bolt app is running!")
+})()
